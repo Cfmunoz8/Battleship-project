fa303429e610790f77eda1fa64cb27d7bc35d4dc
Cfmunoz8 <cfmunoz8@gmail.com>
2023-04-18 23:57:25 +0000

    solo visual

--- battleship/src/components/Boats.js
+++ battleship/src/components/Boats.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, {useRef} from "react";
 
 function Boats(){
+    const refBoats = useRef(null)
+    const refDestroyer = useRef(null)
     const flip = () => {
-        console.log(optionContainer)
+        const rotateShip = refDestroyer
+ //       let angle = angle === 0 ? 90 : 0
+        {rotateShip.style.transform = `rotate(90deg)`}
     } 
     return (
         <div>
-        <div className= "options">
-            <div className="destroyer-preview destroyer" draggable="true"></div>
+        <div className= "options" ref={refBoats}>
+            <div className="destroyer-preview destroyer" ref={refDestroyer} draggable="true"></div>
             <div className="submarine-preview submarine" draggable="true"></div>
             <div className="cruiser-preview cruiser" draggable="true"></div>
             <div className="battleship-preview battleship" draggable="true"></div>
